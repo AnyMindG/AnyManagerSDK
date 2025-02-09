ed8883092b3092b5894cd54700955a43dc31f590
narender <111370321+AnyMindG@users.noreply.github.com>
2025-02-09 19:57:17 +0530

    Updating Swift Package to 1.0.0

---
+++ Package.swift
@@ -0,0 +1,55 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+// v1.0.0
+let package = Package(
+    name: "AnyManagerSDK",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(
+            name: "AnyManagerSDK",
+            targets: ["AnyManagerSDK"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/AnyMindG/IASDKCore.git", from: "1.3.0"),
+        .package(url: "https://github.com/AnyMindG/GoogleMobileAdsMediationFyber.git", from: "1.3.0"),
+        .package(url: "https://github.com/AnyMindG/InMobiAdSDK.git", from: "1.1.0"),
+        .package(url: "https://github.com/AnyMindG/GoogleMobileAdsMediationInMobi.git", from: "1.2.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.13.0"),
+    ],
+    targets: [
+        .target(
+            name: "AnyManagerSDK",
+            dependencies: [
+                .product(name: "IASDKCore", package: "IASDKCore"),
+                .product(name: "GoogleMobileAdsMediationFyber", package: "GoogleMobileAdsMediationFyber"),
+                .product(name: "InMobiAdSDK", package: "InMobiAdSDK"),
+                .product(name: "GoogleMobileAdsMediationInMobi", package: "GoogleMobileAdsMediationInMobi"),
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
+                ],
+            linkerSettings: [
+                .linkedFramework("WebKit"),
+                .linkedFramework("AdSupport"),
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("CoreGraphics"),
+                .linkedFramework("CoreMedia"),
+                .linkedFramework("CoreTelephony"),
+                .linkedFramework("StoreKit"),
+                .linkedFramework("SystemConfiguration"),
+                .linkedFramework("UIKit"),
+                .linkedFramework("AudioToolbox"),
+                .linkedFramework("CFNetwork"),
+                .linkedFramework("MediaPlayer"),
+                .linkedFramework("QuartzCore"),
+                
+            ]
+        ),
+        .testTarget(
+            name: "AnyManagerSDKTests",
+            dependencies: ["AnyManagerSDK"]
+        ),
+    ]
+)
