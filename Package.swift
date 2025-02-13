c0a500c27eec831dedaa9fbf66650bdb5eeac52f
narender <111370321+AnyMindG@users.noreply.github.com>
2025-02-13 14:39:54 +0530

    Updating Swift Package to 1.6.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-// v1.5.0
+// v1.6.0
 let package = Package(
     name: "AnyManagerSDK",
     platforms: [.iOS(.v12)],
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/AnyMindG/AdSDKs.git", from: "1.2.0"),
+        .package(url: "https://github.com/AnyMindG/AdSDKs.git", from: "1.3.0"),
         .package(url: "https://github.com/AnyMindG/OguryMediationGoogleMobileAds.git", from: "1.0.0"),
         .package(url: "https://github.com/AnyMindG/GoogleMobileAdsMediationMintegral.git", from: "1.1.0"),
         .package(url: "https://github.com/AnyMindG/IASDKCore.git", from: "1.3.0"),
@@ -27,7 +27,6 @@ let package = Package(
         .target(
             name: "AnyManagerSDK",
             dependencies: [
-                .product(name: "GoogleMobileAdsMediationMintegral", package: "GoogleMobileAdsMediationMintegral"),
                 .product(name: "MTGSDK", package: "AdSDKs"),
                 .product(name: "MTGSDKBanner", package: "AdSDKs"),
                 .product(name: "MTGSDKBidding", package: "AdSDKs"),
@@ -36,15 +35,25 @@ let package = Package(
                 .product(name: "MTGSDKNewInterstitial", package: "AdSDKs"),
                 .product(name: "MTGSDKReward", package: "AdSDKs"),
                 .product(name: "MTGSDKSplash", package: "AdSDKs"),
+                .product(name: "GoogleMobileAdsMediationMintegral", package: "GoogleMobileAdsMediationMintegral"),
+                
+                .product(name: "AppLovinSDK", package: "AdSDKs"),
+                .product(name: "GoogleMobileAdsMediationAppLovin", package: "AdSDKs"),
+                .product(name: "VungleAds", package: "AdSDKs"),
+                .product(name: "GoogleMobileAdsMediationVungle", package: "AdSDKs"),
+                
                 .product(name: "OguryAds", package: "AdSDKs"),
                 .product(name: "OMSDK_Ogury", package: "AdSDKs"),
                 .product(name: "OguryCore", package: "AdSDKs"),
                 .product(name: "OgurySdk", package: "AdSDKs"),
                 .product(name: "OguryMediationGoogleMobileAds", package: "OguryMediationGoogleMobileAds"),
+                
                 .product(name: "IASDKCore", package: "IASDKCore"),
                 .product(name: "GoogleMobileAdsMediationFyber", package: "GoogleMobileAdsMediationFyber"),
+                
                 .product(name: "InMobiAdSDK", package: "InMobiAdSDK"),
                 .product(name: "GoogleMobileAdsMediationInMobi", package: "GoogleMobileAdsMediationInMobi"),
+                
                 .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
                 ],
             linkerSettings: [
