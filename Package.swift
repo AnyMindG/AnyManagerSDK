6f64f860e278b6525af109f543e1ea332dfda777
narender <anymanagersdk@gmail.com>
2025-07-07 14:24:53 +0530

    Updating AnyManagerSDK Swift Package to 1.17.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-// v1.16.0
+// v1.17.0
 let package = Package(
     name: "AnyManagerSDK",
     platforms: [.iOS(.v12)],
@@ -14,57 +14,23 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/AnyMindG/AdSDKs.git", from: "1.11.0"),
-        .package(url: "https://github.com/AnyMindG/OguryMediationGoogleMobileAds.git", from: "1.0.0"),
-        .package(url: "https://github.com/AnyMindG/GoogleMobileAdsMediationMintegral.git", from: "1.1.0"),
+        .package(url: "https://github.com/AnyMindG/AdSDKs.git", from: "1.12.0"),
         .package(url: "https://github.com/AnyMindG/IASDKCore.git", from: "1.3.0"),
         .package(url: "https://github.com/AnyMindG/GoogleMobileAdsMediationFyber.git", from: "1.3.0"),
-        .package(url: "https://github.com/AnyMindG/InMobiAdSDK.git", from: "1.1.0"),
-        .package(url: "https://github.com/AnyMindG/GoogleMobileAdsMediationInMobi.git", from: "1.2.0"),
         .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.13.0"),
+        
     ],
     targets: [
         .target(
             name: "AnyManagerSDK",
             dependencies: [
-                .product(name: "MTGSDK", package: "AdSDKs"),
-                .product(name: "MTGSDKBanner", package: "AdSDKs"),
-                .product(name: "MTGSDKBidding", package: "AdSDKs"),
-                .product(name: "MTGSDKInterstitialVideo", package: "AdSDKs"),
-                .product(name: "MTGSDKNativeAdvanced", package: "AdSDKs"),
-                .product(name: "MTGSDKNewInterstitial", package: "AdSDKs"),
-                .product(name: "MTGSDKReward", package: "AdSDKs"),
-                .product(name: "MTGSDKSplash", package: "AdSDKs"),
-                .product(name: "GoogleMobileAdsMediationMintegral", package: "GoogleMobileAdsMediationMintegral"),
-                
+                .product(name: "AnyManagerMediation", package: "AdSDKs"),
                 .product(name: "AppLovinSDK", package: "AdSDKs"),
                 .product(name: "GoogleMobileAdsMediationAppLovin", package: "AdSDKs"),
-                .product(name: "VungleAds", package: "AdSDKs"),
-                .product(name: "GoogleMobileAdsMediationVungle", package: "AdSDKs"),
-                
-                .product(name: "OguryAds", package: "AdSDKs"),
-                .product(name: "OMSDK_Ogury", package: "AdSDKs"),
-                .product(name: "OguryCore", package: "AdSDKs"),
-                .product(name: "OgurySdk", package: "AdSDKs"),
-                .product(name: "OguryMediationGoogleMobileAds", package: "OguryMediationGoogleMobileAds"),
-                
-                .product(name: "ChartboostSDK", package: "AdSDKs"),
-                .product(name: "GoogleMobileAdsMediationChartboost", package: "AdSDKs"),
-                .product(name: "ChartboostSDKResources", package: "AdSDKs"),
-                
                 .product(name: "IASDKCore", package: "IASDKCore"),
                 .product(name: "GoogleMobileAdsMediationFyber", package: "GoogleMobileAdsMediationFyber"),
-                
-                .product(name: "InMobiAdSDK", package: "InMobiAdSDK"),
-                .product(name: "GoogleMobileAdsMediationInMobi", package: "GoogleMobileAdsMediationInMobi"),
-                
                 .product(name: "UnityAds", package: "AdSDKs"),
                 .product(name: "GoogleMobileAdsMediationUnity", package: "AdSDKs"),
-                
-                .product(name: "PAGAdSDK", package: "AdSDKs"),
-                .product(name: "GoogleMobileAdsMediationPangle", package: "AdSDKs"),
-                .product(name: "PAGAdSDKResources", package: "AdSDKs"),
-                
                 .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
                 ],
             linkerSettings: [
